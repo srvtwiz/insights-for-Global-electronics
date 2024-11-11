9b768a4bd87eb363d8f6102ccd9e5564b7eceb9e
Viknesh SR <69053295+srvtwiz@users.noreply.github.com>
2024-11-11 19:57:40 +0530

    Delete dataspark_sql directory

--- dataspark_sql/Customer Purchase Frequency.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    "CustomerKey",
-    COUNT("Order Number") AS Purchase_Frequency
-FROM 
-    final_df
-GROUP BY 
-    "CustomerKey"
-ORDER BY 
-    Purchase_Frequency DESC;
--- dataspark_sql/Sales by Store Location.sql
+++
@@ -1,9 +0,0 @@
-SELECT 
-    "State_y",
-    SUM(final_df."Unit Price USD" * final_df."Quantity" * final_df."Exchange") AS Total_Sales
-FROM 
-    final_df
-GROUP BY 
-    "State_y"
-ORDER BY 
-    Total_Sales DESC;
